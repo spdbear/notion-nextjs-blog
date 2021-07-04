8f0409f86143536515740d2358d27a0f8c985f6f
spdbear <16793732+spdbear@users.noreply.github.com>
2021-07-04 22:35:31 +0900

    デフォルト文言を削除

--- pages/index.js
+++ pages/index.js
@@ -7,59 +7,12 @@ export default function Home({ posts }) {
   return (
     <Container>
       <div className="max-w-2xl mx-auto mb-16">
-        <div className="flex justify-center items-center mb-4 max-w-sm mx-auto">
-          <Image src="/notion.svg" height={60} width={60} alt="Notion logo" />
-          <span className="mx-4">+</span>
-          <Image src="/nextjs.svg" height={80} width={133} alt="Next.js logo" />
-          <span className="mx-4">+</span>
-          <Image src="/tailwindcss.svg" height={24} width={192} alt="Tailwind CSS logo" />
-        </div>
         <div className="mb-16">
-          <h1 className="font-bold text-3xl md:text-center w-full md:text-5xl tracking-tight mb-2 text-black mx-auto max-w-xl">
-            Starter blog template powered by Next.js, Notion and Tailwind CSS
+          <h1 className="font-bold text-3xl md:text-center w-full md:text-5xl tracking-tight mb-2 text-black mx-auto max-w-xl leading-loose">
+            spdblog
           </h1>
           <p className="text-gray-700 mb-5 md:text-center max-w-xl mx-auto">
-            This is an open-source starter blog template that is statically generated with{' '}
-            <a
-              href="https://nextjs.org/"
-              className="underline"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Next.js
-            </a>
-            , content powered by{' '}
-            <a
-              className="underline"
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://developers.notion.com/"
-            >
-              Notion
-            </a>
-            , styled with{' '}
-            <a
-              href="http://tailwindcss.com/"
-              className="underline"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Tailwind CSS
-            </a>{' '}
-            and deployed with{' '}
-            <a
-              href="https://vercel.com/"
-              className="underline"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vercel
-            </a>
-            . Grab the source code from{' '}
-            <a className="underline" target="_blank" rel="noopener noreferrer" href="#">
-              Github
-            </a>
-            .
+            これはブログです。
           </p>
         </div>
         <h2 className="font-bold text-2xl md:text-3xl tracking-tight mb-4 mt-8 text-black">
